62cf1e0fffb54e2d1490d0b9f38ed629011d4e91
ksdhandapani <52704352+ksdhandapani@users.noreply.github.com>
2019-09-22 10:44:14 +0530

    Initial Commit

---
+++ src/main/java/com/sakdd/Hibernate03SavingCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.sakdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate03SavingCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate03SavingCollectionsApplication.class, args);
+	}
+
+}
